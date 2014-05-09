7b873c0bd67865119ffce243815ae2d6aa473991
Jonathan Lunt <jml6757@rit.edu>
2014-05-09 09:38:38 -0400

    GetFeatures now extracts features and stores them.

--- scripts/GetFeatures.m
+++ scripts/GetFeatures.m
@@ -1,20 +1,38 @@
 %GetFeatures - Opens an set of images and extracts the features.
 %              This data is saved the folder name.
 
+% Input and output arguments
+INPUT_FOLDER = 'airplanes_side\'
+OUTPUT_FILE = 'plane_features.dat';
 
 % Get the project directory
 ROOT_DIR = strrep(mfilename('fullpath') ,'scripts\GetFeatures','');
 
 % Set the path location and add to the global matlab path
-IMAGE_PATH = strcat(ROOT_DIR, 'images\airplanes_side\');
+IMAGE_PATH = strcat(ROOT_DIR, 'images\', INPUT_FOLDER);
 addpath(IMAGE_PATH);
 
-% Get all file descriptors in the path
-FILES = dir(strcat(IMAGE_PATH, '*.jpg'));
-NUM_FILES = length(FILES);
+% Get all image file descriptors in the path
+FILE_DESCRIPTORS = dir(strcat(IMAGE_PATH, '*.jpg'));
+NUM_FILES = length(FILE_DESCRIPTORS);
+
+% Allocate array to hold extracted features
+FEATURES = struct('filename',  0, 'surf', 0, 'fast', 0, 'brisk', 0);
 
 for i = 1:NUM_FILES
-   IMAGE = imread(FILES(i).name);
+    
+    %Display iteration
+    display(strcat('Image: ', FILE_DESCRIPTORS(i).name));
+    
+    %Load image
+    IMAGE = rgb2gray(imread(FILE_DESCRIPTORS(i).name));
+    
+    %Store extracted features to vector
+    FEATURES(i).filename = FILE_DESCRIPTORS(i).name;
+    FEATURES(i).surf     = detectSURFFeatures(IMAGE);
+    FEATURES(i).fast     = detectFASTFeatures(IMAGE);
+    %FEATURES(i).brisk    = detectBRISKFeatures(IMAGE);
 end
 
-imshow(IMAGE)
+% Store extracted feature vector to disk
+save(strcat(ROOT_DIR,'data\',OUTPUT_FILE), 'FEATURES');
