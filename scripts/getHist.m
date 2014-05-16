372fdc1533e41fc3946518dd6ad55e80ef83e69a
unknown <jxc6857@dco-01.main.ad.rit.edu>
2014-05-16 13:53:09 -0400

    Got rid of old code that didn't work

--- scripts/getHist.m
+++ scripts/getHist.m
@@ -8,24 +8,5 @@ for i = 1:size(features,1)
     [minimum,index(i)] = min(dist(i,:));
 end
 histogram = hist(index, 512);
-
-%     % Get all the distances to the centroids
-%     for i = 1:size(features,1)
-%         for j = 1:size(C,1)
-%             dist(i,j) = norm(features(i,:) - C(j,:));
-%         end
-%     end
-%     
-%     % Assign features to centroids
-%     for i = 1:size(features,1)
-%        [x,I] = min(dist(i,:),[],1);
-%     end
-%     
-%     % Build the histogram
-%     hist = zeros(size(C,1),1);
-%     for i = 1:size(I,1)
-%         hist(I(i)) = hist(I(i)) + 1;
-%     end
-%     hist = hist'; % dont mind me :)
-    
+ 
 end
